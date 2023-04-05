aa8f90a5302ed50b03d4322920af887bb521a4b5
Suraj Mamgai <suraj365311@gmail.com>
2023-04-06 00:47:00 +0530

    Update user.js

--- user.js
+++ user.js
@@ -12,7 +12,7 @@ exports.user_signup = async function (req, res) {
     try {
         const profile_completeness = calculateProfileCompleteness(req.body);
         var result = await db.collection('users').findOne({ username: req.body.username })
-        if (!result)
+        if (result)
         { 
             res.status(400)
             res.send({ "message": "Username Already Taken" })
